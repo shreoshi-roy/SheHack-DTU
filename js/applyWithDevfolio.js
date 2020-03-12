3413414564572734818ab9cc203813ab3c6cdd8b
shreoshi-roy <56217201+shreoshi-roy@users.noreply.github.com>
2020-03-13 02:47:53 +0530

    event postpone

--- js/applyWithDevfolio.js
+++ js/applyWithDevfolio.js
@@ -1,3 +1,5 @@
+alert("We're really sorry to inform you that due to concerns around the coronavirus (COVID-19), and in accordance with health guidance from WHO, and other health authorities, we have decided to postpone SheHacks DTU to a later date. We are overwhelmed by your response and look forward to host you soon, until then stay safe and keep hacking!");
+
 document.addEventListener('DOMContentLoaded', function () {
     let devfolioOptions = {
         buttonSelector: '#devfolio-apply-now',
